62ccb2e43f80e07cb4eea3729eb59af0f5943647
kmalaminstd <alaminkhanstd@gmail.com>
2024-05-21 12:22:58 +0600

    updated some issues

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const productRouter = require("./routes/product.route")
 const dotenv = require("dotenv")
 const app = express()
 
-const port = "https://simple-product-api.vercel.app/"
-
 dotenv.config()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
@@ -24,6 +23,6 @@ app.use("/api/product", productRouter)
 
 
 
-app.listen(port, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log(' ');
 })
--- config/db.js
+++ config/db.js
@@ -2,11 +2,15 @@
 const mongoose = require("mongoose")
 
 
-const database = ()=> mongoose.connect(process.env.mongodbconnection)
-    .then(()=>{
-        console.log('db connected');
-    }).catch(err=>{
-        console.log(err);
-    })
+const database = ()=>{
+
+    mongoose.connect(process.env.MONGODBCONNECTION)
+        .then(()=>{
+            console.log('db connected');
+        }).catch(err=>{
+            console.log(err);
+        })
+
+}
 
 module.exports = database
